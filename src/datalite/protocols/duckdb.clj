bb271de51ed19bba48e90622f3f8b1d6b5be588c
Philipp Küng <git@philippkueng.ch>
2025-09-09 18:46:29 +0200

    re-enable the previously disabled protocol extension for duckdb to play nice with bytearrays

--- src/datalite/protocols/duckdb.clj
+++ src/datalite/protocols/duckdb.clj
@@ -1,9 +1,8 @@
 (ns datalite.protocols.duckdb
-  (:require #_[clojure.java.jdbc :as jdbc]
-    [next.jdbc :as jdbc]))
+  (:require [clojure.java.jdbc :as jdbc]))
 
 ;; Needed as DuckDB doesn't return a ByteArray for the BLOB datatype so we'll need to convert it manually.
-#_(extend-protocol jdbc/IResultSetReadColumn
+(extend-protocol jdbc/IResultSetReadColumn
   org.duckdb.DuckDBResultSet$DuckDBBlobResult
   (result-set-read-column [val _ _]
     (let [buffer-field (.getDeclaredField (class val) "buffer")]
